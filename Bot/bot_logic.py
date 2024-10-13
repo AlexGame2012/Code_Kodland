bf9ffd52362631dd495e4c7de62348b6cafb3378
AlexGame Studio <139368285+AlexGame2012@users.noreply.github.com>
2024-10-13 21:43:28 +0700

    Create bot_logic.py

---
+++ Bot/bot_logic.py
@@ -0,0 +1,12 @@
+import random
+
+def gen_lot():
+        return random.randint(1,10)
+
+        
+def vin_lot():
+        return random.randint(1,10)
+def mon():
+        return random.choice(["Орел", "Решка"])
+def sm():
+        return random.choice([" 😀 ", " 😉 ", " 😳 ", " ⚽ ", " ✡️ ", " 🈚 ", " 😎 ", " 👽 "])
